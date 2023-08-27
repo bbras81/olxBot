117a4087365ad3a9a31dd7967172a132622181db
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2023-08-27 10:18:53 +0200

    Update olxSearch.py

--- olxSearch.py
+++ olxSearch.py
@@ -30,57 +30,55 @@ items = driver.find_elements('xpath', '//*[@data-cy="l-card"]')
 #print(len(items))
 dividir = []
 dataBase = []
+
+
 for i in range(len(items)):
     dividir.append(items[i].text.split('\n'))
     
-    print(dividir)
-    print(len(dividir))
+print(len(dividir))
     
+for i in range(len(dividir)):
     
-    
-
     oneItem = {}
-    match len(dividir[i][0]):
-    
-    
-    case 4:
-        oneItem['top'] = None
-        oneItem['name'] = dividir[i][0]
-        oneItem["price"] = dividir[i][1]
-        oneItem["neg"] = None
-        oneItem['local data'] = dividir[i][2]
-        oneItem["year km"] = dividir[i][3]
-        dataBase.append(oneItem)
-    case 5:
-        if 'TOP' in dividir[0]:
+    match len(dividir[i]):
+        case 4:
+            oneItem['top'] = -1
+            oneItem['name'] = dividir[i][0]
+            oneItem["price"] = dividir[i][1]
+            oneItem["neg"] = -1
+            oneItem['local data'] = dividir[i][2]
+            oneItem["year km"] = dividir[i][3]
+            dataBase.append(oneItem)
+        case 5:
+            if 'TOP' in dividir[0]:
+                oneItem['top'] = dividir[i][0]
+                oneItem['name'] = dividir[i][1]
+                oneItem["price"] = dividir[i][2]
+                oneItem["neg"] = -1
+                oneItem['local data'] = dividir[i][3]
+                oneItem["year km"] = dividir[i][4]
+                dataBase.append(oneItem)
+            else:
+                oneItem['top'] = -1
+                oneItem['name'] = dividir[i][0]
+                oneItem["price"] = dividir[i][1]
+                oneItem["neg"] = dividir[i][2]
+                oneItem['local data'] = dividir[i][3]
+                oneItem["year km"] = dividir[i][4]
+                dataBase.append(oneItem)
+        case 6:
             oneItem['top'] = dividir[i][0]
             oneItem['name'] = dividir[i][1]
             oneItem["price"] = dividir[i][2]
-            oneItem["neg"] = None
-            oneItem['local data'] = dividir[i][3]
-            oneItem["year km"] = dividir[i][4]
+            oneItem["neg"] = dividir[i][3]
+            oneItem['local data'] = dividir[i][4]
+            oneItem["year km"] = dividir[i][5]
             dataBase.append(oneItem)
-        else:
-            oneItem['top'] = None
-            oneItem['name'] = dividir[i][0]
-            oneItem["price"] = dividir[i][1]
-            oneItem["neg"] = dividir[i][2]
-            oneItem['local data'] = dividir[i][3]
-            oneItem["year km"] = dividir[i][4]
-            dataBase.append(oneItem)
-    case 6:
-        oneItem['top'] = dividir[i][0]
-        oneItem['name'] = dividir[i][1]
-        oneItem["price"] = dividir[i][2]
-        oneItem["neg"] = dividir[i][3]
-        oneItem['local data'] = dividir[i][4]
-        oneItem["year km"] = dividir[i][5]
-        dataBase.append(oneItem)
-    #
-    # dividir = []
+
 
 #print(items[1].text)
-print(dataBase)
+#df = pd.DataFrame(dataBase)
+#print(df)
 
 
 
